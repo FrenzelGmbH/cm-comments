a9ce6b6622d092c70ee432caaaa1eb5f053b8462
Philipp Frenzel <philipp@frenzel.net>
2015-03-04 15:54:50 +0100

    work on the comment details

--- views/widgets/views/_index_item.php
+++ views/widgets/views/_index_item.php
@@ -5,57 +5,56 @@
  * @var \yii\web\View $this View
  * @var \net\frenzel\comment\models\frontend\Comment[] $models Comment models
  */
-use net\frenzel\comment\Module;
 use yii\helpers\Url;
 ?>
 <?php if ($models !== null) : ?>
     <?php foreach ($models as $comment) : ?>
         <div class="media" data-comment="parent" data-comment-id="<?= $comment->id ?>">
-            <?php $avatar = $comment->author->profile->avatar_url ? $comment->author->profile->urlAttribute('avatar_url') : Yii::$app->assetManager->publish('@frenzelgmbh/cm-comments/assets/images/blog/avatar3.png')[1]; ?>
+            <?php //$avatar = $comment->author->profile->avatar_url ? $comment->author->profile->urlAttribute('avatar_url') : Yii::$app->assetManager->publish('@frenzelgmbh/cm-comments/assets/images/blog/avatar3.png')[1]; ?>
             <div class="pull-left">
-                <img src="<?= $avatar ?>" class="avatar img-circle width-50" alt="<?= $comment->author->profile->fullName ?>"/>
+                <img src="<?php // $avatar ?>" class="avatar img-circle width-50" alt="<?= $comment->author->profile->name ?>"/>
             </div>
             <div class="media-body">
                 <div class="well" data-comment="append">
                     <div class="media-heading">
-                        <strong><?= $comment->author->profile->fullName ?></strong>&nbsp;
-                        <small><?= $comment->created ?></small>
+                        <strong><?= $comment->author->profile->name ?></strong>&nbsp;
+                        <small><?= $comment->created_at ?></small>
                         <?php if ($comment->parent_id) { ?>
                             &nbsp;
                             <a href="#comment-<?= $comment->parent_id ?>" data-comment="ancor" data-comment-parent="<?= $comment->parent_id ?>"><i class="icon-arrow-up"></i></a>
                         <?php } ?>
-                        <?php if ($comment->isActive) { ?>
+                        <?php if (is_null($comment->deleted_at)) { ?>
                             <div class="pull-right" data-comment="tools">
-                                <?php if (Yii::$app->user->can('createComment')) { ?>
-                                    <a href="#" data-comment="reply" data-comment-id="<?= $comment->id ?>">
-                                        <i class="icon-repeat"></i> <?= Module::t('comment', 'FRONTEND_WIDGET_COMMENT_REPLY') ?>
+                                <?php if (!Yii::$app->user->isGuest) { ?>
+                                    <a href="#" data-comment="reply" data-comment-id="<?= $comment->id ?>" class="btn btn-success">
+                                        <i class="fa fa-reply"></i> <?= \Yii::t('comment', 'antworten') ?>
                                     </a>
                                 <?php } ?>
-                                <?php if (Yii::$app->user->can('updateComment') || Yii::$app->user->can('updateOwnComment', ['model' => $comment])) { ?>
+                                <?php if (Yii::$app->user->identity->isAdmin) { ?>
                                     &nbsp;
                                     <a href="#" data-comment="update" data-comment-id="<?= $comment->id ?>" data-comment-url="<?= Url::to([
                                         '/comment/default/update',
                                         'id' => $comment->id
                                     ]) ?>">
-                                        <i class="icon-pencil"></i> <?= Module::t('comment', 'FRONTEND_WIDGET_COMMENT_UPDATE') ?>
+                                        <i class="fa fa-pencil"></i> <?= \Yii::t('comment', 'bearbeiten') ?>
                                     </a>
                                 <?php } ?>
-                                <?php if (Yii::$app->user->can('deleteComment') || Yii::$app->user->can('deleteOwnComment', ['model' => $comment])) { ?>
+                                <?php if (Yii::$app->user->identity->isAdmin) { ?>
                                     &nbsp;
                                     <a href="#" data-comment="delete" data-comment-id="<?= $comment->id ?>" data-comment-url="<?= Url::to([
                                         '/comment/default/delete',
                                         'id' => $comment->id
-                                    ]) ?>" data-comment-confirm="<?= Module::t('comment', 'FRONTEND_WIDGET_COMMENT_DELETE_CONFIRMATION') ?>">
-                                        <i class="icon-trash"></i> <?= Module::t('comment', 'FRONTEND_WIDGET_COMMENT_DELETE') ?>
+                                    ]) ?>" data-comment-confirm="<?= \Yii::t('comment', 'FRONTEND_WIDGET_COMMENT_DELETE_CONFIRMATION') ?>">
+                                        <i class="fa fa-icon-trash"></i> <?= \Yii::t('comment', 'entfernen') ?>
                                     </a>
                                 <?php } ?>
                             </div>
                         <?php } ?>
                     </div>
-                    <?php if ($comment->isDeleted) { ?>
-                        <?= Module::t('comment', 'FRONTEND_WIDGET_COMMENT_DELETED_COMMENT_TEXT') ?>
+                    <?php if (!is_null($comment->deleted_at)) { ?>
+                        <?= \Yii::t('comment', 'FRONTEND_WIDGET_COMMENT_DELETED_COMMENT_TEXT') ?>
                     <?php } else { ?>
-                        <div class="content" data-comment="content"><?= $comment->content ?></div>
+                        <div class="content" data-comment="content"><?= $comment->text ?></div>
                     <?php } ?>
                 </div>
                 <?php if ($comment->children) { ?>
