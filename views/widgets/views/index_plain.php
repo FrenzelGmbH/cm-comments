57d68ef44c24341c82ff5af5adf66d5fe144a24a
Philipp Frenzel <pf@com-x-cha.com>
2018-01-17 20:38:56 +0100

    Clean up the plain comments to only display comments without surrounding

--- views/widgets/views/index_plain.php
+++ views/widgets/views/index_plain.php
@@ -10,24 +10,12 @@
 ?>
 
 <div id="comment">
-
-<div class="panel panel-default">
-	<div class="panel-heading">
-		<h4 class="panel-title">
-			<i class="fa fa-comments-o"></i> 
-			<?= \Yii::t('comment', 'Kommentar') ?>
-		</h4>
-	</div>
-	<div class="panel-body">
-		<?php if (!\Yii::$app->user->isGuest) : ?>	        
-	        <?= $this->render('_form_plain', ['model' => $model]); ?>
-    	<?php endif; ?>		
-		<!--/ #comment-list -->
-		<div id="comment-list" data-comment="list">
-	        <?= $this->render('_index_item', ['models' => $models]) ?>
-	    </div>
-    	<!--/ #comment-list -->		
-	</div>	
-</div>
-    
+   <?php if (!\Yii::$app->user->isGuest) : ?>	        
+   <?= $this->render('_form_plain', ['model' => $model]); ?>
+   <?php endif; ?>		
+    <!--/ #comment-list -->
+    <div id="comment-list" data-comment="list">
+    <?= $this->render('_index_item', ['models' => $models]) ?>
+    </div>
+    <!--/ #comment-list -->		    
 </div>
